c6b4db6bfe677c9f303d5b22fcdde48db2fc93f9
Frederic <fredericclaus@user601.alpaca.int>
2019-03-25 16:07:02 +0100

    view aangemaakt

--- examenDjango/TimeDifference/views.py
+++ examenDjango/TimeDifference/views.py
@@ -3,3 +3,20 @@ from datetime import datetime
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotFound
 
 # Create your views here.
+
+def index(request):
+    
+    dateNow = datetime.now().astimezone
+    DateBrexit = datetime(2019,3,29,11)
+
+    diff = DateBrexit - dateNow
+
+    days, seconds = diff.days, diff.seconds
+    hours = days * 24 + seconds // 3600
+    minutes = (seconds % 3600) // 60
+    seconds = seconds % 60
+
+    total = hours,"h " ,minutes,"m " ,seconds, "s"
+
+
+    return HttpResponse(total)
